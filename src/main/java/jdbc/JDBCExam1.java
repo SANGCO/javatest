4a7035734e27dc42e926c58f35d75a1d74ae85b6
SANGCO <nsg1213@gmail.com>
2020-11-05 13:02:05 +0900

    jdbc

---
+++ src/main/java/jdbc/JDBCExam1.java
@@ -0,0 +1,11 @@
+package jdbc;
+
+public class JDBCExam1 {
+
+    public static void main(String[] args) {
+        RoleDao dao = new RoleDao();
+        Role role = dao.getRole(100);
+        System.out.println(role);
+    }
+
+}
