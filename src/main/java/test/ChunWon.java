47f524bcd127205b6edf57dd70323061f271c129
SANGCO <nsg1213@gmail.com>
2021-02-09 13:08:32 +0900

    콤마 찍기 메서드 쓰는걸로 수정

--- src/main/java/test/ChunWon.java
+++ src/main/java/test/ChunWon.java
@@ -1,12 +1,30 @@
 package test;
 
 import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.Stack;
 
 public class ChunWon {
 
     public static void main(String[] args) {
-        String solve = solve(new BigInteger("10000000000000000000"));
+        String solve = solve2(new BigInteger("10000000000000000000"));
         System.out.println(solve);
+    }
+
+    private static String solve2(BigInteger numb) {
+        char[] chars = numb.toString().toCharArray();
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            sb.append(chars[i]);
+            count++;
+            if (count == 3) {
+                sb.append(",");
+                count = 0;
+            }
+        }
+
+        return sb.reverse().toString();
     }
 
     private static String solve(BigInteger numb) {
